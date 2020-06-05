c163b71c44aeaadf5042a0935cd1a1ce51b333c8
Özgür Koca <ozgurkoca@gmail.com>
2020-06-05 03:28:35 +0300

    status, debug panel ve catch node'a hata gonderen surum

---
+++ hesap-makinesi.js
@@ -0,0 +1,31 @@
+module.exports = function(RED) {
+    function HesapMakinesiNode(config) {
+        RED.nodes.createNode(this, config);
+
+        var node = this;
+        node.on('input', function(msg) {
+
+			try {
+				const x = 5;
+
+				var msg1 = {}, msg2 = {}, msg3 = {}, msg4 = {};
+
+	            msg1.payload = Number(msg.sayi1) + Number(msg.sayi2);
+				msg2.payload = Number(msg.sayi1) - Number(msg.sayi2);
+				msg3.payload = Number(msg.sayi1) * Number(msg.sayi2);
+				msg4.payload = Number(msg.sayi1) / Number(msg.sayi2);
+
+				x = 8;
+
+			} catch (error) {
+				node.status( {fill:"red", shape:"dot", text:"Sabite deger atama hatası!"} );
+				node.error("Debug Panel: Sabite deger atama hatasi!");
+				throw("Throw: Sabite deger atama hatasi!");
+
+			} finally {
+	            node.send([msg1, msg2, msg3, msg4]);
+			}
+        });
+    }
+    RED.nodes.registerType("hesap-makinesi", HesapMakinesiNode);
+}
